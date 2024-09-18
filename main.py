d142a3d32e39793052eabc9c16204e5676ea9240
Tatiana Snook <tatsnook@gmail.com>
2024-09-18 13:31:15 -0500

    classroom activity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def select_dish(foods, selected_food):
+    if selected_food <= 0:
+        raise IndexError("Pick a number between 1 and 5")
     print(f"Ah, {foods[selected_food]}! An excellent choice!")
 
 def your_menu(foods):
@@ -7,13 +9,18 @@ def your_menu(foods):
         for dish in foods:
             print(f"{index}. {dish}")
             index += 1
-    
+   
         selected_choice = int(input("Your order number? "))
+
         select_dish(foods, selected_choice - 1)
     except IndexError as error:
+        print(f"{selected_choice} was entered. {error}")
+        # print("Next time try entering something on the menu! Between 1 and 5")
+    except ValueError as error:
         print(f"{error} was entered.")
-        print("Next time try entering something on the menu!")
+        print("Enter a number, please.")
 
+    
 menu_items = [
     "Yakisoba",
     "Pho Tai Nam Gan",
